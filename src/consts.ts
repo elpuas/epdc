fa3e5474261b20054952fb11da6a06fab9e831a3
Alfredo Navas <elpuas@gmail.com>
2025-05-21 08:33:02 -0600

    On branch feature/SEO  Changes to be committed: 	new file:   public/default.png 	modified:   src/components/BaseHead.astro 	modified:   src/consts.ts 	modified:   src/i18n/translations/en.json 	modified:   src/pages/en/about.astro 	modified:   src/pages/en/contact.astro 	modified:   src/pages/en/crafted-with-love/index.astro 	modified:   src/pages/en/index.astro 	modified:   src/pages/en/services.astro 	modified:   src/pages/es/about.astro 	modified:   src/pages/es/contact.astro 	modified:   src/pages/es/crafted-with-love/index.astro 	modified:   src/pages/es/index.astro 	modified:   src/pages/es/services.astro 	modified:   src/pages/it/about.astro 	modified:   src/pages/it/contact.astro 	modified:   src/pages/it/crafted-with-love/index.astro 	modified:   src/pages/it/index.astro 	modified:   src/pages/it/services.astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,7 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'ElPuas Digital Crafts | Web Development with Love and Precision';
+export const SITE_DESCRIPTION = 'We build fast, affordable, and scalable websites using WordPress, Astro, and AI-powered workflows. Trusted by agencies and small businesses around the world.';
+export const SUPPORTED_LANGUAGES = ['en', 'es', 'it'];
+export const DEFAULT_LANGUAGE = 'en';
