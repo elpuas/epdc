10c821354581645fe7c266c297cdbaf50eb93df3
Alfredo Navas <elpuas@gmail.com>
2025-05-20 18:14:14 -0600

    On branch feature/portfolio  Your branch is up to date with 'origin/feature/portfolio'.  Changes to be committed: 	modified:   astro.config.mjs 	modified:   src/pages/en/contact.astro 	modified:   src/pages/en/crafted-with-love/index.astro 	new file:   src/styles/Contact.module.css 	modified:   src/styles/Footer.module.css 	deleted:    src/styles/GridLayout.module.css 	modified:   src/styles/Header.module.css 	modified:   src/styles/LanguageSwitcher.module.css 	modified:   src/styles/Layout.module.css 	modified:   src/styles/MobileDock.module.css

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import { DEFAULT_LANGUAGE, SUPPORTED_LANGUAGES } from './src/i18n/utils';
 
 /** @type {import('astro').AstroUserConfig} */
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://elpuasdigitalcrafts.com',
 	integrations: [
 		mdx(), 
 		sitemap(), 
